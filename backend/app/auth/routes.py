bfd6a466185919239c1c00b2290cef5f0ce426aa
francescodc <francesco.decesare001@studenti.uniparthenope.it>
2026-06-05 10:41:18 +0200

    server authentication

--- backend/app/auth/routes.py
+++ backend/app/auth/routes.py
@@ -1,14 +1,72 @@
-from flask import jsonify, request
+from flask import jsonify, request, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from app.auth import bp
 from config.database import users_collection
 
-
 # the route for the login request received
 @bp.route('/login', methods=['POST'])
 def login():
-    return jsonify({"message": "Login Endpoint Ready"})
+    # recovering json data from the request
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No data provided"})
+
+    email = data.get("email","").strip()
+    password = data.get("password","").strip()
+
+    # checking the presence of all data
+    if not email or not password:
+        return jsonify({"error": "missing an obligatory camp"})
+
+    # searching user in MongoDB database
+    user = users_collection.find_one({"email":email})
+
+    # checking is user exists and if password is the right one
+    if not user or not check_password_hash(user["password_hash"], password):
+        return jsonify({"error": "invalid credentials"}), 401
+
+    # saving user important information in flask session
+    session.clear() # clear old session eventually
+    session["user_id"] = str(user["_id"]) # recovering MongoDB ObjectID and converting into a string
+    session["email"] = user["email"] # recovering user email
+    session["roles"] = user["roles"] # recovering user roles
+
+    # update the user last login on database
+    users_collection.update_one(
+        {"_id": user["_id"]},
+        {"$set": {"last_access": datetime.now(timezone.utc)}},
+    )
+
+    # successful response to frontend
+    return jsonify({
+        "success": True,
+        "message": "Logged in successfully",
+        "user": {
+            "first_name": user["first_name"],
+            "last_name": user["last_name"],
+            "email": user["email"],
+            "roles": user["roles"],
+        }
+    }), 200
+
+# method is post for avoiding cross-site-request-forgery (CSRF)
+@bp.route('/logout', methods=['POST'])
+def logout():
+    # check if the user has an active session
+    if "user_id" not in session:
+        return jsonify(
+            {"success": False},
+            {"error": "no active session found, impossible to logout"}
+        ), 400
+    # clearing the current session, deleting all data stored in it
+    session.clear()
+
+    # respond to the frontend for a successful logout
+    return jsonify(
+        {"success": True},
+        {"message": "Logged out successfully"}
+    ),200
 
 # the route for the registration request received
 @bp.route('/register', methods=['POST'])
@@ -16,7 +74,7 @@ def register():
     # recovering json data from the request
     data = request.get_json()
     if not data:
-        return jsonify({"message": "No data provided"})
+        return jsonify({"error": "No data provided"})
 
     email = data.get("email","").strip()
     password = data.get("password","")
@@ -26,16 +84,16 @@ def register():
 
     # check data received
     if not email or not password or not first_name or not last_name:
-        return jsonify({"error": "missing an obligatory camp"})
+        return jsonify({"error": "missing an obligatory camp"}), 400
 
     # role check, default fallback for the data is student
     # but a security check is done anyway
     if role not in ["student","tutor"]:
-        return jsonify({"error": "role must be student or tutor"})
+        return jsonify({"error": "role must be student or tutor"}), 400
 
     # check if the user exists already by email
     if users_collection.find_one({"email":email}):
-        return jsonify({"error": "email already exists"})
+        return jsonify({"error": "email already exists"}), 409
 
     # generating password hashing
     password_hash = generate_password_hash(password)
