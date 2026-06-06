d1e9c3a18881383c463b7516bb7d31666ae1e13b
francescodc <francesco.decesare001@studenti.uniparthenope.it>
2026-06-06 12:10:18 +0200

    added CORS info

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from settings import Setting
 
 app = Flask("Connect")
 app.config.from_object(Setting)
+
+#configuring CORS (Cross-Origin-Resource-Sharing) to let know who can comunicate with the server
+frontend_url = app.config.get('FRONTEND_URL')
+CORS(app, origins=[frontend_url])
 
 # blueprints registration
 # authentication
--- backend/settings.py
+++ backend/settings.py
@@ -2,3 +2,4 @@ import os
 
 class Setting:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    FRONTEND_URL = os.environ.get('FRONTEND_URL')
