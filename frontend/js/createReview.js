b5c94844093e0007c2afed69502226246e807f01
cosimocampitiello <cosimo.campitiello001@studenti.uniparthenope.it>
2026-07-07 17:36:35 +0200

    Update for review and home_page

---
+++ frontend/js/createReview.js
@@ -0,0 +1,79 @@
+import { API_URL } from "../app.js";
+
+let goTo;
+
+export function init(page, navigateTo){
+
+    if(navigateTo){
+        goTo = navigateTo;
+    }
+
+    if(page === "writeReview"){
+
+        const reviewForm = document.getElementById("reviewForm");
+
+        if(reviewForm){
+            reviewForm.addEventListener("submit", handleReview);
+        }
+
+        const backButton = document.getElementById("backButton");
+
+        if(backButton){
+            backButton.addEventListener("click", () =>{
+                goTo("main")
+            });
+        }
+    }
+}
+
+
+async function handleReview(event) {
+
+    event.preventDefault();
+
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+        alert("you must be logged in");
+
+        if (goTo) {
+            goTo("main");
+        }
+        return;
+    }
+
+    // change this when the control of classroom it's finished
+    const email = document.getElementById("TutorEmail").value.trim();
+    const comment = document.getElementById("comment").value.trim();
+    const rating = Number(document.getElementById("rating").value);
+
+    if(rating === 0){
+        alert("Please select a rating");
+        return;
+    }
+
+    try{
+        const response = await fetch(`${API_URL}/review`,{
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                email: email,
+                rating: rating,
+                comment: comment,
+            })
+        });
+
+        const data = await response.json();
+
+        if(response.ok){
+            document.getElementById("reviewForm").reset();
+        }else{
+            alert(data.message);
+        }
+    }catch(error){
+        alert("Impossible to connect to server")
+    }
+}
